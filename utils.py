06010e91153124c948ec5b1992cf5bc38339ce5b
zachary <syyun@snu.ac.kr>
2020-01-12 17:34:27 +0900

    Add standard plotting of the data

--- utils.py
+++ utils.py
@@ -3,14 +3,33 @@ import datetime
 import numpy as np
 import pandas as pd
 
+import matplotlib.pyplot as plt
+
 
 def convert_to_datetime(time: str):
     return datetime.datetime.strptime(time[:9], "%Y %m ").date()
 
 
-def clean_EIA_series(data: pd.DataFrame):
+def clean_EIA_series(data: pd.DataFrame, column_label: str):
     data.replace('-', np.nan, regex=True, inplace=True)
     data.fillna(method='bfill', inplace=True)
     data.index = data.index.map(convert_to_datetime)
     data.index = pd.to_datetime(data.index)
+    data.columns = [column_label]
     return None  # best-practice to denote the function is void.
+
+
+def plot_time_series(data: pd.DataFrame, column_name: str = "price",
+                     scatter: bool = False):
+
+    plt.figure(figsize=(12, 6), dpi=100)
+    if not scatter:
+        plt.plot(data.index,
+                 data[column_name])
+    if scatter:
+        plt.scatter(data.index,
+                    data[column_name])
+    plt.xlabel('Date')
+    plt.ylabel(column_name)
+    plt.grid()
+    plt.show()
