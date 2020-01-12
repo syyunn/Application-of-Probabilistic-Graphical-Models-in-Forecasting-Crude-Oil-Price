83205c68d569d268cddc68a4f317a4813e4ac685
zachary <syyun@snu.ac.kr>
2020-01-12 17:10:01 +0900

    Add utils for cleanse the data as a wrapper of cleaning process

--- EIA/data.py
+++ EIA/data.py
@@ -3,6 +3,7 @@ import numpy as np
 
 import eia
 
+import utils
 import api_keys
 
 if __name__ == "__main__":
@@ -10,4 +11,12 @@ if __name__ == "__main__":
     time_series_label = 'TOTAL.COEXPUS.M'  # (C)rude (O)il (E)xport - (M)onthly
     eia_data = pd.DataFrame(eia_api.data_by_series(
         series=time_series_label))
+
+    indices_datetime = eia_data.index.map(utils.convert_to_datetime)
+    pd_df_indices_datetime = pd.to_datetime(indices_datetime)
+    eia_data.index = pd.to_datetime(pd_df_indices_datetime)  # index re-write
+    eia_data.columns = [time_series_label]  # feature-label re-write
+
+    eia_data.replace('-', np.nan, regex=True, inplace=True)
+    eia_data.fillna(method='bfill', inplace=True)  # Backward fill of na
     pass
--- utils.py
+++ utils.py
@@ -1,5 +1,15 @@
 import datetime
 
+import numpy as np
+import pandas as pd
+
 
 def convert_to_datetime(time: str):
     return datetime.datetime.strptime(time[:9], "%Y %m ").date()
+
+
+def clean_EIA_series(data: pd.DataFrame):
+    data.replace('-', np.nan, regex=True, inplace=True)
+    data.fillna(method='bfill', inplace=True)
+    data.index = data.index.map(convert_to_datetime)
+    data.index = pd.to_datetime(data.index)
